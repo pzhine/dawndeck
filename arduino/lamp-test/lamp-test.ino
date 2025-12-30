3d2aa664969ea304068adeabb28e5ce02c52dba3
Paul Hine <paul.hine@gmail.com>
2025-12-30 18:54:42 +0100

    Update lamp-test.ino

--- arduino/lamp-test/lamp-test.ino
+++ arduino/lamp-test/lamp-test.ino
@@ -37,26 +37,20 @@ void setup() {
   strip.show();
 }
 void loop() {
-
+  breathe(10);
   //rainbow(10);             // Flowing rainbow cycle along the whole strip
 }
 
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    // strip.rainbow() can take a single argument (first pixel hue) or
-    // optionally a few extras: number of rainbow repetitions (default 1),
-    // saturation and value (brightness) (both 0-255, similar to the
-    // ColorHSV() function, default 255), and a true/false flag for whether
-    // to apply gamma correction to provide 'truer' colors (default true).
-    strip.rainbow(firstPixelHue);
-    // Above line is equivalent to:
-    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
+// Breathe brightness from 0 to 255 and back
+void breathe(int wait) {
+  for (int b = 0; b < 256; b++) {
+    strip.setBrightness(b);
+    strip.show();
+    delay(wait);
+  }
+  for (int b = 255; b >= 0; b--) {
+    strip.setBrightness(b);
+    strip.show();
+    delay(wait);
   }
 }
